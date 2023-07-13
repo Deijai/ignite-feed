956359f2037786cb82e4dba8e71004b12f73799d
Deijai Miranda Almeida <djairn18@gmail.com>
2023-07-13 19:08:18 -0300

    Finalizando a aplicação

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,28 @@
+/* eslint-disable react/prop-types */
 import { ThumbsUp, Trash } from "@phosphor-icons/react";
 import styles from "./Comment.module.css";
 import { Avatar } from "./Avatar";
-export function Comment() {
+import { useState } from "react";
+export function Comment({ content, onDeleteComment }) {
+
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount( ( state ) => {
+        return state + 1;
+    });
+  }
+
   return (
     <div className={styles.comment}>
-      <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/14796276?v=4" />
+      <Avatar
+        hasBorder={false}
+        src="https://avatars.githubusercontent.com/u/14796276?v=4"
+      />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
@@ -16,17 +34,17 @@ export function Comment() {
               </time>
             </div>
 
-            <button title="Deletar comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={24} />
             </button>
           </header>
-          <p>Muito bom Devon, parabéns 🫠</p>
+          <p> {content} 🫠</p>
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,56 +1,112 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
 import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
-export function Post() {
+//date
+import { format, formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+
+export function Post({ author, publishedAt, content }) {
+  const [comments, setComments] = useState(["Post Muito bom, parabéns!"]);
+  const [newComment, setNewComment] = useState("");
+
+  const publishedDateFormatted = format(
+    publishedAt,
+    "d 'de' LLLL 'ás' HH:mm'h'",
+    { locale: ptBR }
+  );
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  });
+
+  function handleCreateNewComment(ev) {
+    ev.preventDefault();
+    setComments([...comments, newComment]);
+    setNewComment("");
+  }
+
+  function handleNewCommentChange(ev) {
+    ev.target.setCustomValidity('');
+    setNewComment(ev.target.value);
+  }
+
+  function deleteComment(comment) {
+    //criar uma nova lista de cometários
+    const commentsWithoutDeletedOne = comments.filter( c => c !== comment );
+    setComments(commentsWithoutDeletedOne);
+  }
+
+  function handleNewCommentInvalid(ev) {
+    return ev.target.setCustomValidity('Esse campo é obrigatório');
+  }
+
+  const isNewCommentEmpty = newComment.length === 0;
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar
-            src="https://avatars.githubusercontent.com/u/14796276?v=4"
-          />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Almeida Miranda Deijaí</strong>
-            <span>Mobile Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title="12 de julho de 2023" dateTime="2023-07-12 08:20:00">
-          Publicado há 1h
+        <time
+          title={publishedDateFormatted}
+          dateTime={publishedAt.toISOString()}
+        >
+          {publishedDateRelativeToNow}
         </time>
       </header>
 
       <div className={styles.content}>
-        <p>#SoundsLike (United States) ✋</p>
-        <p>
-          For Pandora (@pandoramusic), there is bound to be a piece of music for
-          each emoji. At least this is what the American music streaming giant
-          wanted to prove with its campaign #SoundsLike.
-        </p>
-        <p>
-          {" "}
-          👉 <a href="#">design.com.br/design</a>
-        </p>
-        <p>
-          <a href="#">#novoprojeto</a> <a href="#">#web</a>{" "}
-          <a href="#">#reactjs</a> <a href="#">#dev</a>{" "}
-        </p>
+        {content.map((line) => {
+          if (line.type === "paragraph") {
+            return <p key={line.content}> {line.content} </p>;
+          } else {
+            return (
+              <a key={line.content} href="#">
+                {" "}
+                {line.content}{" "}
+              </a>
+            );
+          }
+        })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feddback</strong>
 
-        <textarea placeholder="Dexe um comentário"></textarea>
+        <textarea
+          name="comment"
+          required
+          onInvalid={handleNewCommentInvalid}
+          onChange={handleNewCommentChange}
+          value={newComment}
+          placeholder="Dexe um comentário"
+        ></textarea>
         <footer>
-          <button type="submit">Publicar</button>
+          <button disabled={isNewCommentEmpty} type="submit">
+            Publicar
+          </button>
         </footer>
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map((comment) => {
+          return (
+            <Comment
+              key={comment}
+              content={comment}
+              onDeleteComment={deleteComment}
+            />
+          );
+        })}
       </div>
     </article>
   );
